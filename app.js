fa05d5006b4102a1c3a9b65baa407f4709736607
Protosac <protosacq@gmail.com>
2014-12-14 16:04:15 -0800

    Add profile page data

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express       = require('express'),
     ejs           = require('ejs'),
     path          = require('path'),
     request    		= require('request'),
+    oauth 				= require('oauth'),
     bodyParser    = require('body-parser'),
     cookieParser  = require('cookie-parser'),
     session       = require('express-session'),
@@ -10,6 +11,7 @@ var express       = require('express'),
     methodOverride= require('method-override'),
     db            = require('./db.js');
     app           = express(),
+    twitterAPI 		= require('node-twitter-api'),
     port          = process.env['PORT'] || 7000;
 
 // CONFIGURATION
@@ -63,7 +65,43 @@ var localStrategy = new LocalStrategy(
 
 passport.use(localStrategy);
 
-// ROUTES: All Users
+// Configure Twitter authentication
+// var twitter = new twitterAPI({
+//     consumerKey: 'SHojLsO5Xo0ab3GoLvAX2Kefg',
+//     consumerSecret: 'PIbEX0KAi60QbBhPe1ilEhcybt6OpgpFsIwbwb3M6I5Eb1vDtD',
+//     callback: 'http://localhost:7000'
+// });
+
+// twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
+//     if (error) {
+//         console.log("Error getting OAuth request token : " + error);
+//     } else {
+//         //store token and tokenSecret somewhere, you'll need them later; redirect user
+//     }
+// });
+
+// twitter.getAccessToken(requestToken, requestTokenSecret, oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         //store accessToken and accessTokenSecret somewhere (associated to the user)
+//         //Step 4: Verify Credentials belongs here
+//     }
+// });
+
+// twitter.verifyCredentials(accessToken, accessTokenSecret, function(error, data, response) {
+//     if (error) {
+//         //something was wrong with either accessToken or accessTokenSecret
+//         //start over with Step 1
+//     } else {
+//         //accessToken and accessTokenSecret can now be used to make api-calls (not yet implemented)
+//         //data contains the user-data described in the official Twitter-API-docs
+//         //you could e.g. display his screen_name
+//         console.log(data["screen_name"]);
+//     }
+// });
+
+// ROUTES: ALL USERS =================================================
 app.get('/', function(req,res) {
   res.render('index', { user: req.user });
 });
@@ -76,6 +114,17 @@ app.get('/signup', function(req,res) {
   res.render('signup', { user: req.user });
 });
 
+// Navbar search form
+app.get('/results', function(req,res) {
+  var params = req.query['search'];
+  request('https://api.twitter.com/1.1/search/tweets.json?q=%23' + params, function(error, response, body) {
+    var protests = JSON.parse(body);
+    console.log(protests);
+    res.render('results', { user: req.user, protests: protests });
+  });
+});
+
+// User sign-up submission with redirect to login form.
 app.post('/signup', function(req,res) {
   var registration = [req.body.username, req.body.email, req.body.password];
   db.query("INSERT INTO users (username, email, password) VALUES ($1, $2, $3)", registration, function(err, dbRes) {
@@ -85,11 +134,13 @@ app.post('/signup', function(req,res) {
   });
 });
 
+// User login authentication with redirect to homepage
 app.post('/login', passport.authenticate('local', 
   {failureRedirect: 'login'}), function(req, res) {
     res.redirect('/');
 });
 
+// User logout
 app.get('/logout', function(req,res) {
   res.render('index', { user: req.user });
 });
@@ -99,6 +150,15 @@ app.delete('/logout', function(req, res) {
   res.redirect('index');
 });
 
-// ROUTES: Registered Users
+// ROUTES: REGISTERED USERS ====================================================
+
+app.get('/profile', function(req,res) {
+	var user = req.user;
+	if (user) {
+  	res.render('user/profile', { user: user });
+	} else {
+		res.send('You must be logged in.');
+	}
+});
 
 // ROUTES: Organizers
