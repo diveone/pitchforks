dadb2c0a86f9219cc0e2f9eee4c6668a2ba8c168
Protosac <protosacq@gmail.com>
2014-12-16 08:54:49 -0800

    Add more DRYness, update DB

--- routes/handlers.js
+++ routes/handlers.js
@@ -1,5 +1,5 @@
 // Declare DB, since won't work from application.js
-var db = require('../db.js');
+var db            = require('../db.js');
 var passport      = require('passport');
 
 // **********************
@@ -58,7 +58,76 @@ exports.addSignup = function addSignup(req,res) {
   });
 };
 
+// 1.LOGIN NOT LOADING -- 2. STORE COOKIE IN DB?
 exports.userLogin =  function userLogin(req,res) {
-	passport.authenticate('local', {failureflash: 'Try again.'}),
-	function(req,res) { res.redirect('/'); }
+	passport.authenticate('local', {failureRedirect: 'login'}),
+	function(err,dbRes) { res.redirect('index'); }
 };
+
+// LOGOUT
+exports.logout = function logout(req,res) {
+  req.logout();
+  res.redirect('index');
+};
+
+// ************************************
+// USERS ONLY -- REQUIRES AUTHORIZATION
+// ************************************
+
+// EDIT USER FORM - TEST
+exports.editPage = function editPage(req,res) {
+  var user = req.user;
+  res.render('users/edit', { user: user });
+};
+
+// EDIT USER DB UPDATE - TEST
+exports.editUser = function editUser(req,res) {
+  var userData = [req.body.username, req.body.email, req.body.avatar, req.params.id];
+  db.query("UPDATE users SET username = $1, email = $2, avatar = $3 WHERE id = $4", userData, function(err, dbRes) {
+    if (!err) {
+      res.redirect('/profile');
+    }
+  });
+};
+
+// ADD PROTEST FORM - ***AUTHENTICATE** - TEST 
+exports.newProtest = function newProtest(req,res) {
+  var user = req.user;
+  if (user) {
+    res.render('protests/new', { user: user });
+  } else {
+    res.render('login');
+  }
+};
+
+// PROTEST DB INSERT - TEST 
+exports.addProtest = function addProtest(req,res) {
+  var protestData = [req.body.name, req.body.description, req.body.location, req.body.date, req.user.id];
+  db.query("INSERT INTO protests (name, description, location, date, submitted_by) VALUES ($1, $2, $3, $4, $5)", protestData, function(err, dbRes) {
+    if(!err) {
+      res.redirect('/');
+      // Needs redirect to the protest submitted
+    }
+  });
+}
+
+// EDIT PROTEST FORM - ***AUTHENTICATE*** - TEST 
+exports.editProtest = function editProtest(req,res) {
+  var user = req.user;
+  if (user) {
+    res.render('protests/edit', { user: user });
+  } else {
+    res.render('login');
+  }
+};
+
+// PROTEST DB UPDATE - TEST 
+exports.updateProtest = function updateProtest(req,res) {
+  var protestData = [req.body.name, req.body.location, req.body.date, req.params.id];
+  db.query("UPDATE protests SET name = $1, location = $2, date = $3 WHERE event_id = $4", protestData, function(err, dbRes) {
+    if (!err) {
+      res.redirect('protests/:id');
+    }
+  });
+};
+
--- routes/routes.js
+++ routes/routes.js
@@ -8,10 +8,20 @@ module.exports = function(app) {
 	app.get('protests/:id'	, publicRoutes.show);
 
 	// TWITTER NAVBAR SEARCH
-	app.get('/results', publicRoutes.results);
+	app.get('/results'			, publicRoutes.results);
 
 	// FORMS
 	app.post('/signup'			, publicRoutes.addSignup);
 	app.post('/login'				, publicRoutes.userLogin);
+	app.delete('/logout'		, publicRoutes.logout);
+
+	// USERS ONLY -- AUTHORIZATION REQUIRED
+	app.get('/edit'					, publicRoutes.editPage);
+	app.patch('users/:id'		,	publicRoutes.editUser);
+
+	app.get('/protests'			, publicRoutes.newProtest);
+	app.post('/submit'			, publicRoutes.addProtest);
+	app.get('/protests/edit', publicRoutes.editProtest);
+	app.patch('/protests/:id',publicRoutes.updateProtest);
 
 }
