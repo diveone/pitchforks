796a7fa569a5a51a77a522e702e3af01c4e665d4
Protosac <protosacq@gmail.com>
2014-12-15 17:31:45 -0800

    Updates

--- app.js
+++ app.js
@@ -11,12 +11,25 @@ var express       = require('express'),
     passport      = require('passport'),
     methodOverride= require('method-override'),
     db            = require('./db.js');
+    flash         = require('connect-flash'),
     app           = express(),
     util = require('util'),
     TwitterStrategy = require('passport-twitter').Strategy,
     // twitterAPI 		= require('node-twitter-api'),
     // RedisStore    = require('connect-redis')(express),
-    port          = process.env['PORT'] || 7000;
+    port          = process.env['PORT'] || 8000;
+
+var OAuth= require('oauth').OAuth;
+
+var oa = new OAuth(
+  "https://api.twitter.com/oauth/request_token",
+  "https://api.twitter.com/oauth/access_token",
+  "SHojLsO5Xo0ab3GoLvAX2Kefg",
+  "PIbEX0KAi60QbBhPe1ilEhcybt6OpgpFsIwbwb3M6I5Eb1vDtD",
+  "1.0",
+  "http://localhost:8000/auth/twitter/callback",
+  "HMAC-SHA1"
+);
 
 // CONFIGURATION
 app.set('view engine', 'ejs');
@@ -27,12 +40,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':true}));
 app.use(session({
   secret: 'milk and cookies',
-  cookie: { maxAge: 300000 },
   resave: false,
   saveUninitialized: true
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.configure(function() {
+  app.use(express.cookieParser('keyboard cat'));
+  app.use(express.session({ cookie: { maxAge: 60000 }}));
+  app.use(flash());
+});
 
 // PORT
 app.listen(port, function() {
@@ -153,7 +171,7 @@ app.post('/signup', function(req,res) {
 
 // If login credentials are in database, success. Else, login again.
 app.post('/login', passport.authenticate('local', 
-  {failureRedirect: 'login', failureFlash: 'No dice. Try again.'}), function(req, res) {
+  {failureRedirect: 'login' }), function(req, res) {
     res.redirect('/');
 });
 
@@ -255,19 +273,62 @@ app.patch('/protests/:id', function(req, res) {
 });
 
 // ===================================================================
-// TWITTER AUTHENTICATION: https://github.com/jaredhanson/passport-twitter
+// TWITTER AUTHENTICATION: http://moonlitscript.com/post.cfm/how-to-use-oauth-and-twitter-in-your-node-js-expressjs-app/
 // ===================================================================
-app.get('/auth/twitter', passport.authenticate('twitter'), function(req, res){
-    // The request will be redirected to Twitter for authentication, so this
-    // function will not be called.
+app.get('/auth/twitter', function(req, res){
+  oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
+    if (error) {
+      console.log(error);
+      res.send("yeah no. didn't work.")
+    }
+    else {
+      req.session.oauth = {};
+      req.session.oauth.token = oauth_token;
+      console.log('oauth.token: ' + req.session.oauth.token);
+      req.session.oauth.token_secret = oauth_token_secret;
+      console.log('oauth.token_secret: ' + req.session.oauth.token_secret);
+      res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token)
+    }
   });
+});
 
-app.get('/auth/twitter/callback', 
-  passport.authenticate('twitter', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
+var userResults;
+
+app.get('/auth/twitter/callback', function(req,res) {
+  if (req.session.oauth) {
+    req.session.oauth.verifier = req.query.oauth_verifier;
+    var oauth = req.session.oauth;
+
+    oa.getOAuthAccessToken(oauth.token,oauth.token_secret,oauth.verifier, 
+    function(error, oauth_access_token, oauth_access_token_secret, results){
+      if (error){
+        console.log(error);
+        res.send("yeah something broke.");
+      } else {
+        req.session.oauth.access_token = oauth_access_token;
+        req.session.oauth.access_token_secret = oauth_access_token_secret;        
+        console.log(results);
+        userResults = [results.user_id, results.screen_name];
+        console.log(userResults);
+        res.render("twitter", { user: req.user });
+      }
+    });
+  } else {
+    res.send("you're not supposed to be here.");
+  }
+})
+
+app.get('/twitter', function(req,res) {
+  res.render('twitter', { user: req.user });
+});
+
+app.post('/twitter', function(req,res) {
+  var params = [userResults[0],userResults[1], req.body.email, req.body.password];
+  db.query('INSERT INTO users (twitter_id, username, email, password) VALUES ($1, $2, $3, $4)', params, function(err,dbRes) {
+    console.log(userResults[0]);
+    res.render("login", { user: req.user } );
   });
-
+});
 // ===================================================================
 // MIDDLEWARE
 // ===================================================================
