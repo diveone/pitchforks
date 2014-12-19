7d2ff630e9c406d61b8d73200ccf9c72e706e099
Protosac <protosacq@gmail.com>
2014-12-18 17:11:38 -0800

    Update db.js

--- db.js
+++ db.js
@@ -2,15 +2,31 @@
 var db = {};
 var pg = require('pg');
 
-db.config = {
-  database: "pitchforks",
-  port: 5432,
-  host: "localhost",
-  user: "postgres"
-};
+// db.config = {
+//   database: "pitchforks",
+//   port: 5432,
+//   host: "localhost",
+//   user: "postgres"
+// };
 
+// HEROKU
+db.config = {}
+
+// db.connect = function(runAfterConnecting) {
+//   pg.connect(db.config, function(err, client, done){
+//     if (err) {
+//       console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
+//     }
+//     runAfterConnecting(client);
+//     done();
+//   });
+// };
+
+// HEROKU
 db.connect = function(runAfterConnecting) {
-  pg.connect(db.config, function(err, client, done){
+  console.log(process.env.DATABASE_URL);
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
     if (err) {
       console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
     }
