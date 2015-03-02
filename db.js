3b557a629f714c4ff7421e2455884faecb881cd3
Protosac <protosacq@gmail.com>
2015-03-02 10:51:10 -0800

    Updates

--- db.js
+++ db.js
@@ -3,22 +3,22 @@ var db = {};
 var pg = require('pg');
 
 // LOCALHOST
-// db.config = {
-//   database: "pitchforks",
-//   port: 5432,
-//   host: "localhost",
-//   // user: "postgres"
-// };
+db.config = {
+  database: "pitchforks",
+  port: 5432,
+  host: "localhost",
+  // user: "postgres"
+};
 
-// db.connect = function(runAfterConnecting) {
-//   pg.connect(db.config, function(err, client, done){
-//     if (err) {
-//       console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
-//     }
-//     runAfterConnecting(client);
-//     done();
-//   });
-// };
+db.connect = function(runAfterConnecting) {
+  pg.connect(db.config, function(err, client, done){
+    if (err) {
+      console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
+    }
+    runAfterConnecting(client);
+    done();
+  });
+};
 
 // LOCALHOST AND HEROKU
 db.query = function(statement, params, callback){
@@ -27,20 +27,20 @@ db.query = function(statement, params, callback){
   });
 };
 
-HEROKU
-db.config = {}
+// HEROKU
+// db.config = {}
 
-db.connect = function(runAfterConnecting) {
-  console.log(process.env.DATABASE_URL);
+// db.connect = function(runAfterConnecting) {
+//   console.log(process.env.DATABASE_URL);
 
-  pg.connect(process.env.DATABASE_URL, function(err, client, done){
-    if (err) {
-      console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
-    }
-    runAfterConnecting(client);
-    done();
-  });
-};
+//   pg.connect(process.env.DATABASE_URL, function(err, client, done){
+//     if (err) {
+//       console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
+//     }
+//     runAfterConnecting(client);
+//     done();
+//   });
+// };
 
 
 // Heroku DB Link
